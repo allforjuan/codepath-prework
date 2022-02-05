38c44c815eb0fc09d0699d111a5f346e72174c61
Juan <juan.martinez63@myhunter.cuny.edu>
2022-02-04 19:39:19 -0500

    improved UI, added stepper for multiple party payment and labels to show calculation per person

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -14,25 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var partySize: UILabel!
+    @IBOutlet weak var groupStepper: UIStepper!
+    @IBOutlet weak var partyAmountLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Sets the title in the Navigation Bar
+        self.title = "Tip Calculator"
+        
+        // value of stepper changes
+        groupStepper.wraps = true
+        groupStepper.autorepeat = true
+        groupStepper.maximumValue = 10
     }
-
+    public var total = 0.0
     @IBAction func calculateTip(_ sender: Any) {
-        //Get bill amount from text field input
+        // Get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
         // Get Total tip by multiplying tip * tipPercentage
         let tipPercentages = [0.15, 0.18, 0.2]
         let tip = bill *
             tipPercentages[tipControl.selectedSegmentIndex]
-        let total = bill + tip
+        total = bill + tip
         
-        //update Tip Amount Label
+        // update Tip Amount Label
         tipAmountLabel.text = String(format: "$%.2f", tip)
-        //update Total Amount
+        // update Total Amount
         totalLabel.text = String(format: "$%.2f", total)
     }
     
+    
+    @IBAction func groupValueChanged(_ sender: UIStepper) {
+        partySize.text = Int(sender.value).description
+    }
+    
 }
-
